05229f4daa5243365151a3680deb89c2fe60137a
Zahid Rouf <zahid19@gmail.com>
2020-12-05 23:18:22 +0600

    Update practice_array2d.c

--- practice_array2d.c
+++ practice_array2d.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-const int TOTA_STUDENT = 2;
-const int TOTAL_COLUMN = 4;
-int mark[TOTA_STUDENT][TOTAL_COLUMN];
 int main()
 {
-    for (int i = 0; i < CITY; ++i)
+    const int TOTAL_STUDENT = 2;
+    const int TOTAL_COLUMN = 4;
+    int mark[TOTAL_STUDENT][TOTAL_COLUMN];
+    for (int i = 0; i < TOTAL_STUDENT; ++i)
     {  
         printf("ENTER STUDENT ID:\n");
         scanf("%d", &mark[i][0]);
         printf("ENTER Mark:\n");
-        for (int j = 1; j < 4; ++j)
+        for (int j = 1; j < TOTAL_COLUMN; ++j)
         {           
             scanf("%d", &mark[i][j]);
         }
     }
-    for (int i = 0; i < CITY; ++i)
+    for (int i = 0; i < TOTAL_STUDENT; ++i)
     {  
         printf("STUDENT ID: %d\n", mark[i][0] );        
         printf("Mark:");
-        for (int j = 1; j < 4; ++j)
+        for (int j = 1; j < TOTAL_COLUMN; ++j)
         {
             printf("%d ", mark[i][j] );            
         }
